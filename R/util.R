ad8ee14cc824ac7f2355c2c58d3972ff91092a16
wilsonfreitas <wilson.freitas@gmail.com>
2017-02-04 06:05:03 -0200

    new function strip_numeric
    This function strips a number into a vector of digits.

--- R/util.R
+++ R/util.R
@@ -1,6 +1,13 @@
 
 extract_digit <- function(x, n) x %/% 10^(n-1) %% 10
 
+strip_numeric <- function(x) {
+  x <- as.character(x)
+  lx <- Map(function(ix) substr(x, ix, ix), seq_len(nchar(x)))
+  as.integer(unlist(lx))
+}
+
+
 #' @export
 is.valid <- function(x) UseMethod('is.valid', x)
 
